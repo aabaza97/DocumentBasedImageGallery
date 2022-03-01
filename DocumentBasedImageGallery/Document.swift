77ce3c7fd1733465d34bd3cad58b0fc778e311b0
Ahmed Abaza <aabaza32@gmail.com>
2022-03-01 11:27:23 +0200

    Initial Commit

---
+++ DocumentBasedImageGallery/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  DocumentBasedImageGallery
+//
+//  Created by Ahmed Abaza on 01/03/2022.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
